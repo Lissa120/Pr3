568a60df171529086ec47d247ead0755bffe1e1a
админ <@DESKTOP-7OIMLR3>
2022-11-14 11:39:26 +0500

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public delegate void AccountHandler(Account sender, AccountEventArgs e);
+
+        public class Account
+        {
+            public event AccountHandler Notify;
+            public int sum { get; private set; }
+            public string fio;
+            // Создаем переменную делегата
+            AccountHandler taken;
+            public Account(int sum, string fio)
+            {
+                this.sum = sum;
+                this.fio = fio;
+            }
+
+
+            public void RegisterHandler(AccountHandler del)
+            {
+                taken += del;
+            }
+
+
+
+            public void Add(int sum)
+            {
+                this.sum += sum;
+                Notify?.Invoke(this, new AccountEventArgs($"На счет поступило: {sum} руб.", sum));
+            }
+
+            public void Take(int sum)
+            {
+                if (this.sum >= sum)
+                {
+                    this.sum -= sum;
+                    // вызываем делегат, передавая ему сообщение
+                    Notify?.Invoke(this, new AccountEventArgs($"Сумма {sum} снята со счета", sum));
+                }
+                else
+                {
+                    Notify?.Invoke(this, new AccountEventArgs("Недостаточно денег на счете", sum));
+                }
+            }
+        }
+        public class AccountEventArgs
+        {
+            // Сообщение
+            public string Message { get; }
+            // Сумма, на которую изменился счет
+            public int Sum { get; }
+            public AccountEventArgs(string message, int sum)
+            {
+                Message = message;
+                Sum = sum;
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static ClassLibrary1.Class1;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Account accc;
+        void DisplayMessage(Account sender, AccountEventArgs e)
+        {
+            MessageBox.Show($"Владелец счета: {accc.fio}\nСумма транзакции: {e.Sum} руб.\n"+ e.Message+$"\nТекущая сумма на счете: {sender.sum} руб.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(textBox3.Text);
+             if (accc.sum < x)
+             {
+                 accc.Notify += DisplayMessage;   // Добавляем обработчик для события Notify
+                 listBox1.Items.Clear();
+                 listBox1.Items.Add("На счету недостаточно средств");
+                 accc.Notify -= DisplayMessage;   // Добавляем обработчик для события Notify
+                new ToastContentBuilder()
+                .AddText($"На счету недостаточно средств")
+                .Show();
+            }
+             else
+             {
+                 accc.Notify += DisplayMessage;   // Добавляем обработчик для события Notify
+                 accc.Take(Convert.ToInt32(textBox3.Text));
+                 listBox1.Items.Clear();
+                 listBox1.Items.Add($"Владелец: {accc.fio}, счет: {accc.sum}");
+                 accc.Notify -= DisplayMessage;   // Добавляем обработчик для события Notify
+                new ToastContentBuilder()
+                .AddText($"Владелец счета: {accc.fio}, счет: {accc.sum}")
+                .Show();
+            }
+        }
+
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            accc.Notify += DisplayMessage;   // Добавляем обработчик для события Notify
+            accc.Add(Convert.ToInt32(textBox3.Text));
+            listBox1.Items.Clear();
+            listBox1.Items.Add($"Владелец: {accc.fio}, счет: {accc.sum}");
+            accc.Notify -= DisplayMessage;   // Добавляем обработчик для события Notify
+                                             
+            new ToastContentBuilder()
+                .AddText($"Владелец: {accc.fio}, счет: {accc.sum}")
+                .AddText($"К счету добавленно {accc.sum}")
+                .Show();
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            accc = new Account(Convert.ToInt32(textBox2.Text), textBox1.Text);
+            listBox1.Items.Clear();
+            listBox1.Items.Add($"Владелец: {accc.fio}, счет: {accc.sum}");
+            button2.Visible = true;
+            button3.Visible = true;
+            textBox3.Visible = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
